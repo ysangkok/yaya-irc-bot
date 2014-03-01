942235ca7440c9549b8fa7a5bdb12c1a81ea687b
nobuoka <nobuoka@vividcode.info>
2014-03-01 17:06:17 +0900

    インクリメントに反応するプラグイン

---
+++ plugins/IncDecCounterListener.groovy
@@ -0,0 +1,25 @@
+package info.vividcode.yaya.plugin.listeners
+
+import org.pircbotx.PircBotX
+import org.pircbotx.hooks.ListenerAdapter
+import org.pircbotx.hooks.events.MessageEvent
+
+class IncDecCounterListener extends ListenerAdapter<PircBotX> {
+
+    def counters = [:]
+
+    /**
+     * スレッドセーフ
+     */
+    @Override
+    synchronized void onMessage(MessageEvent<PircBotX> event) {
+        def m = event.getMessage() =~ /(\S+)\+\+/
+        if (m.matches()) {
+            def targetName = m.group(1)
+            if (counters[targetName] == null) counters[targetName] = 0
+            def count = ++counters[targetName]
+            event.getChannel().send().notice("${targetName}: ${count}")
+        }
+    }
+
+}
