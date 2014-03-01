3190c5bb1880a56bb1137dd4c1696947c506cfa1
nobuoka <nobuoka@vividcode.info>
2014-03-01 23:00:22 +0900

    Groovy スクリプトを実行するプラグイン

---
+++ plugins/GroovyExecutionListener.groovy
@@ -0,0 +1,78 @@
+package info.vividcode.yaya.plugin.listeners
+
+import groovy.transform.ThreadInterrupt
+
+import java.util.concurrent.atomic.AtomicBoolean
+
+import org.codehaus.groovy.control.CompilerConfiguration
+import org.codehaus.groovy.control.customizers.ASTTransformationCustomizer
+
+import org.pircbotx.PircBotX
+import org.pircbotx.hooks.ListenerAdapter
+import org.pircbotx.hooks.events.MessageEvent
+
+class GroovyExecutionListener extends ListenerAdapter<PircBotX> {
+
+    /**
+     * スレッドセーフ
+     */
+    @Override
+    synchronized void onMessage(MessageEvent<PircBotX> event) {
+        def m = event.getMessage() =~ /\Agroovy:(.+)\z/
+        if (m.matches()) {
+            def code = m.group(1)
+            executeGroovyCodeWithTimeout(code, event, 2000)
+        }
+    }
+
+    private void executeGroovyCodeWithTimeout(final String code, final MessageEvent<PircBotX> event, final long timeoutInMs) {
+        def lockObj = new Object()
+        def timeouted = false
+        def tFinished = false
+        def t2Finished = false
+        final def t = new Thread() {
+            @Override void run() {
+                try {
+                    def config = new CompilerConfiguration()
+                    config.addCompilationCustomizers(new ASTTransformationCustomizer(ThreadInterrupt))
+                    def res = new GroovyShell(config).evaluate(new GroovyCodeSource(code, "RestrictedScript", "/restrictedScript"))
+                    event.getChannel().send().notice("${res}".replaceAll("\\s+", " "))
+                } catch (Throwable err) {
+                    event.getChannel().send().notice("『${err.message}』 ってエラーが出たよ＞＜".replaceAll("\\s+", " "))
+                }
+                synchronized(lockObj) {
+                    tFinished = true
+                }
+            }
+        }
+        t.start()
+        final def t2 = new Thread() {
+            @Override void run() {
+                try {
+                    Thread.sleep(timeoutInMs)
+                    synchronized(lockObj) {
+                        if (!tFinished) {
+                            t.interrupt()
+                            timeouted = true
+                        }
+                        t2Finished = true
+                    }
+                } catch (InterruptedException err) {
+                    // 入力された処理が早く終わったらここに来る
+                }
+            }
+        }
+        t2.start()
+        t.join()
+        synchronized(lockObj) {
+            if (!t2Finished) t2.interrupt()
+        }
+        t2.join()
+        synchronized(lockObj) {
+            if (timeouted) {
+                event.getChannel().send().notice("ヽ^ｼ＞ω＜)ﾉｼ 時間かかりすぎてるからタイムアウトさせてあげたの".replaceAll("\\s+", " "))
+            }
+        }
+    }
+
+}
