5269ff96b13279e9e052417aae131943ae6056ea
nobuoka <nobuoka@vividcode.info>
2014-03-07 01:41:45 +0900

    Real name ではなく id と nick を使う
    Real name だと、あとからログインしたユーザーのものが空白になってしまう。

--- plugins/NaritaiList.groovy
+++ plugins/NaritaiList.groovy
@@ -1,6 +1,7 @@
 package info.vividcode.yaya.plugin.listeners
 
 import org.pircbotx.PircBotX
+import org.pircbotx.User
 import org.pircbotx.hooks.ListenerAdapter
 import org.pircbotx.hooks.events.MessageEvent
 
@@ -15,40 +16,53 @@ class NaritaiListener extends ListenerAdapter<PircBotX> {
     synchronized void onMessage(MessageEvent<PircBotX> event) {
         def m = event.getMessage() =~ /(.*\S)\s*になりたい/
         if (m.matches()) {
-            def targetUserName = event.user.realName
+            def targetUserName = event.user.nick
+            def targetUserId = event.user.userId
             def naritaiMono = m.group(1)
-            if (userToNaritaiMonoListMap[targetUserName] == null) {
-                userToNaritaiMonoListMap[targetUserName] = []
+            if (userToNaritaiMonoListMap[targetUserId] == null) {
+                userToNaritaiMonoListMap[targetUserId] = []
             }
-            userToNaritaiMonoListMap[targetUserName] << naritaiMono
+            userToNaritaiMonoListMap[targetUserId] << naritaiMono
             event.getChannel().send().notice("${targetUserName} さんがなりたいもの...φ(＞ω＜ )ﾒﾓﾒﾓ")
         }
 
         m = event.getMessage() =~ /(.*\S)\s*になりました/
         if (m.matches()) {
-            def targetUserName = event.user.realName
+            def targetUserName = event.user.nick
+            def targetUserId = event.user.userId
             def naritaiMono = m.group(1)
-            def naritaiListForUser = userToNaritaiMonoListMap[targetUserName]
+            def naritaiListForUser = userToNaritaiMonoListMap[targetUserId]
             if (naritaiListForUser != null) {
                 int initSize = naritaiListForUser.size()
-                userToNaritaiMonoListMap[targetUserName] = naritaiListForUser.dropWhile { it.equals(naritaiMono) }
-                if (naritaiListForUser.size() != userToNaritaiMonoListMap[targetUserName].size()) {
+                userToNaritaiMonoListMap[targetUserId] = naritaiListForUser.dropWhile { it.equals(naritaiMono) }
+                if (naritaiListForUser.size() != userToNaritaiMonoListMap[targetUserId].size()) {
                     event.getChannel().send().notice("なりたいものになれておめでたいです ヾ(＞ヮ＜*)")
                 }
-                if (userToNaritaiMonoListMap[targetUserName].size() == 0) {
-                    userToNaritaiMonoListMap[targetUserName] = null
+                if (userToNaritaiMonoListMap[targetUserId].size() == 0) {
+                    userToNaritaiMonoListMap[targetUserId] = null
                 }
             }
         }
 
         m = event.getMessage() =~ /(?:(.*?\S)\s*(?:さん)?(?:の|が))?なりたいもの(?:は\?|リスト)?/
         if (m.matches()) {
-            def targetUserName = m.group(1) ?: event.user.realName
-            if (userToNaritaiMonoListMap[targetUserName] != null) {
-                def res = userToNaritaiMonoListMap[targetUserName].join(" とか ")
-                event.getChannel().send().notice("${targetUserName} さんは ${res} になりたいみたいです。")
+            def targetUserName = m.group(1) ?: event.user.nick
+            def targetUserId = null
+            for (User u : event.channel.users) {
+                if (u.nick.equals(targetUserName)) {
+                    targetUserId = u.userId
+                    break
+                }
+            }
+            if (targetUserId != null) {
+                if (userToNaritaiMonoListMap[targetUserId] != null) {
+                    def res = userToNaritaiMonoListMap[targetUserId].join(" とか ")
+                    event.getChannel().send().notice("${targetUserName} さんは ${res} になりたいみたいです。")
+                } else {
+                    event.getChannel().send().notice("${targetUserName} さんは何になりたいんでしょうか ヽ^ｼ▰╹ヮ╹)ﾉｼ")
+                }
             } else {
-                event.getChannel().send().notice("${targetUserName} さんは何になりたいんでしょうか ヽ^ｼ▰╹ヮ╹)ﾉｼ")
+                event.channel.send().notice("${targetUserName} さんって誰ですか???")
             }
         }
     }
